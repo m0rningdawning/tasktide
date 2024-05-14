9bae9bce0db1c3ad530dc4ecba9819c41309851a
Pavlo Korolov <bibonga54@gmail.com>
2024-05-14 21:49:31 +0200

    Organizations + half working middleware

--- app/front-end/tasktide-fe/middleware.ts
+++ app/front-end/tasktide-fe/middleware.ts
@@ -1,7 +1,43 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { authMiddleware, clerkMiddleware, createRouteMatcher, redirectToSignIn } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+// const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up']);
+
+// export default authMiddleware({
+//   publicRoutes: ["/", "/sign-in", "/sign-up"],
+//   afterAuth(auth:any, req:any) {
+//     if (auth.userId && auth.isPublicRoute) {
+//       let path = "/select-org";
+
+//       if (auth.orgId) {
+//         path = `/organization/${auth.orgId}`;
+//       }
+
+//       const orgSelection = new URL(path, req.url);
+//       return NextResponse.redirect(orgSelection);
+//     }
+//     if (!auth.userId && !auth.isPublicRoute) {
+//       return redirectToSignIn({ returnBackUrl: req.url });
+//     }
+
+//     if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+//       const orgSelection = new URL("/select-org", req.url);
+//       return NextResponse.redirect(orgSelection);
+//     }
+//   },
+// });
+
+// export const config = {
+//   // matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { clerkMiddleware } from '@clerk/nextjs/server';
+
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
