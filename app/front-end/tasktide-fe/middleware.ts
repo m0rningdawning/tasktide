f5d9fa086d867d6cba997b7caa7c9bd5a59f897f
Pavlo Korolov <bibonga54@gmail.com>
2024-05-28 23:34:15 +0200

    Middleware enabled

--- app/front-end/tasktide-fe/middleware.ts
+++ app/front-end/tasktide-fe/middleware.ts
@@ -1,40 +1,39 @@
-// import { authMiddleware, clerkMiddleware, createRouteMatcher, redirectToSignIn } from "@clerk/nextjs/server";
-// import { NextResponse } from "next/server";
+import { authMiddleware, clerkMiddleware, createRouteMatcher, redirectToSignIn } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-// const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up']);
+const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up']);
 
-// export default authMiddleware({
-//   publicRoutes: ["/", "/sign-in", "/sign-up"],
-//   afterAuth(auth:any, req:any) {
-//     if (auth.userId && auth.isPublicRoute) {
-//       let path = "/select-org";
+export default authMiddleware({
+  publicRoutes: ["/", "/sign-in", "/sign-up"],
+  afterAuth(auth:any, req:any) {
+    if (auth.userId && auth.isPublicRoute) {
+      let path = "/select-org";
 
-//       if (auth.orgId) {
-//         path = `/organization/${auth.orgId}`;
-//       }
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
 
-//       const orgSelection = new URL(path, req.url);
-//       return NextResponse.redirect(orgSelection);
-//     }
-//     if (!auth.userId && !auth.isPublicRoute) {
-//       return redirectToSignIn({ returnBackUrl: req.url });
-//     }
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
 
-//     if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
-//       const orgSelection = new URL("/select-org", req.url);
-//       return NextResponse.redirect(orgSelection);
-//     }
-//   },
-// });
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  },
+});
 
 // export const config = {
 //   // matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 //   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 // };
 
-import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware()
+// export default clerkMiddleware()
 
 export const config = {
   // The following matcher runs middleware on all routes
